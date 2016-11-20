b24fbaa62c749872cbc5111c58f983a54844ccb7
dj1792 <djayaswa@andrew.cmu.edu>
2016-11-19 23:34:34 -0500

    Skills3

---
+++ Gemfile
@@ -0,0 +1,18 @@
+source 'https://rubygems.org'
+
+gem 'sinatra'
+gem 'activesupport'
+
+gem "rake"
+gem 'activerecord'
+gem 'sinatra-activerecord' # excellent gem that ports ActiveRecord for Sinatra
+
+gem 'json'
+gem 'haml'
+gem 'builder'
+
+gem 'twilio-ruby'
+
+gem 'dotenv'
+gem 'pg'
+gem 'instagram'
---
+++ app.rb
@@ -0,0 +1,100 @@
+require 'sinatra'
+require "active_support/all"
+require 'sinatra/activerecord'
+require 'json'
+require 'rake'
+
+#before do
+#	content_type :json
+#end
+
+require 'twilio-ruby'
+require 'instagram'
+
+require_relative './models/link'
+
+enable :sessions
+
+configure :development do
+  require 'dotenv'
+  Dotenv.load
+end
+
+client = Twilio::REST::Client.new ENV["Twilio_sid"], ENV["Twilio_token"]
+
+get "/send_sms" do
+  client.account.messages.create(
+    :from => "+14122183432",
+    :to => "+14122947286",
+    :body => "Hey there. This is a test"
+  )
+
+  "Sent message"
+end
+
+get '/incoming_sms' do
+
+  session["counter"] ||= 0
+  count = session["counter"]
+  
+  sender = params[:From] || ""
+  body = params[:Body] || ""
+  body = body.downcase.strip
+
+  link = Link.where(placeholder: body)
+  
+  if link	
+  	message = link.msg  
+  else
+ 	message = "Not valid, try who, why, what or where"   
+  end
+  
+  session["counter"] += 1
+  
+  twiml = Twilio::TwiML::Response.new do |r|
+    r.Message message
+  end
+
+  content_type 'text/xml'
+
+  twiml.text
+
+end
+
+#returns the overall most popular media items from Instagram
+#http://www.rubydoc.info/gems/instagram/1.1.6--> reference
+get "/instagram" do
+  Instagram.configure do |config|
+  config.client_id = ENV["insta_id"]
+  config.client_secret = ENV["insta_secret"]
+  end
+  
+  Instagram.authorize_url
+
+  response = Instagram.get_access_token(params[:code],)
+  session[:access_token] = response.access_token
+
+  client = Instagram.client(:access_token => session[:access_token])
+ 
+  results = client.media_popular(limit:3)
+  message = "Most popular image on Instagram"
+  pic = results.first.media_item.images.thumbnail.url
+    
+  twiml = Twilio::TwiML::Response.new do |r|
+    r.Message do |m|
+        m.Body message
+        unless pic.nil?
+            m.Media pic
+        end
+    end
+  end
+  twiml.text
+end
+
+get'/' do
+	error 401
+end
+
+error 401 do
+	{ error: "Not allowed"}.to_json
+end
